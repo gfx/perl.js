bc65316461bfdba7b227257667023b77a6469621
Fuji, Goro <goro-fuji@cookpad.com>
2013-09-19 12:15:37 +0900

    reload the web page after abort()

--- prelude.js
+++ prelude.js
@@ -6,3 +6,71 @@ var Module = {
 	//preRun:  [], // called before every run()
 };
 
+if (typeof jQuery !== "undefined") { // for the web
+	jQuery(function($) {
+		function output(s) {
+			var output = $('#output');
+			output.val(output.val() + s + "\n");
+		}
+		function onOutput(c) {
+			var o = $('#output');
+			o.val(o.val() + String.fromCharCode(s));
+		}
+		function onInput() {
+			return null;
+		}
+		Module.stdin  = onInput;
+		Module.stdout = onOutput;
+		Module.stderr = onOutput;
+		Module.print  = output;
+		Module.printErr = output;
+		Module.exit = function exit(status) {
+			EXITSTATUS = status;
+			STACKTOP = initialStackTop;
+			throw new ExitStatus(status);
+		};
+
+		$('#output').val(sessionStorage.getItem('output'));
+
+		$('#eval').click(function() {
+			sessionStorage.removeItem('output');
+			$('#output').val('');
+			preloadStartTime = null;
+			ABORT = false;
+			try {
+				try { FS.unlink('/input') } catch (e) { }
+				FS.createDataFile('/', 'input', $('#input').val(), true, true);
+				Module.callMain(['/input']);
+			} catch (err) {
+				output(err.message || err.toString());
+			}
+			finally {
+				var src = encodeURIComponent($('#input').val());
+				location.hash = src;
+			}
+		});
+
+		var src = location.hash;
+		if (src) {
+			$('#input').val(decodeURIComponent(src.slice(1)));
+			$('#eval').trigger('click');
+		}
+		else {
+			$('#input').val(
+				"use strict;\n" +
+				"use warnings;\n" +
+				"\n" +
+				"print \"Hello, perl.js!\\n\"\n");
+			Module.callMain(['--version'])
+		}
+
+
+		// monky patch
+		Module.abort = abort = function abort(text) {
+			alert('abort!');
+			sessionStorage.setItem('output', $('#output').val());
+			window.location.reload();
+			Module.exit(1);
+		};
+	});
+}
